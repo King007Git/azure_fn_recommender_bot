a03a64b5610f1ab94eec4fe948f5d4bef71411b3
King007Git <yourkingiam007@gmail.com>
2026-05-28 17:17:04 +0530

    animation modified

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -4,12 +4,11 @@ import { Toaster } from "@/components/ui/sonner";
 export default function Layout() {
   return (
     <div className="relative flex h-screen w-screen flex-col overflow-hidden bg-[#fdfcfd]">
-      
       <div className="pointer-events-none absolute inset-0 z-0 overflow-hidden">
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-pink-50/30 to-transparent" />
-        <div className="absolute -bottom-[20%] -left-[10%] h-[70%] w-[60%] animate-float-pink rounded-full bg-pink-300/40 blur-[120px]" />
-        <div className="absolute -bottom-[20%] -right-[10%] h-[70%] w-[60%] animate-float-indigo rounded-full bg-indigo-300/30 blur-[120px]" />
-        <div className="absolute top-[10%] left-[20%] h-[50%] w-[60%] animate-float-fuchsia rounded-full bg-fuchsia-200/30 blur-[120px]" />
+        <div className="absolute -bottom-[20%] -left-[10%] h-[70%] w-[60%] animate-float-pink rounded-full bg-pink-300 blur-[120px]" />
+        <div className="absolute -bottom-[20%] -right-[10%] h-[70%] w-[60%] animate-float-indigo rounded-full bg-indigo-300 blur-[120px]" />
+        <div className="absolute top-[10%] left-[20%] h-[50%] w-[60%] animate-float-fuchsia rounded-full bg-fuchsia-200 blur-[120px]" />
       </div>
 
       <Outlet />
